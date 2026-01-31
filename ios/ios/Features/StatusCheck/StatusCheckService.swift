eb67ffd1830305d139e7857a48e6bb2b25f71d93
slomako <s.lomako@venga.com>
2026-01-31 20:22:48 +0300

    ios - fix tests

--- ios/ios/Features/StatusCheck/StatusCheckService.swift
+++ ios/ios/Features/StatusCheck/StatusCheckService.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct StatusCheckService {
     func getStatus() async throws -> StatusCheckModel {
-        let url = URL(string: "http://192.168.64.24/health")!
+        let url = URL(string: "http://127.0.0.1:8080/health")!
         let (data, _) = try await URLSession.shared.data(from: url)
         return try StatusCheckParser.parse(data: data)
     }
