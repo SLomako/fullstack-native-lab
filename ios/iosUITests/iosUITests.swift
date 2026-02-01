4e55d5bb598d87009c9d282a308bca6ac7098de4
slomako <s.lomako@venga.com>
2026-02-01 14:30:25 +0300

    ios - ci3/ref27

--- ios/iosUITests/iosUITests.swift
+++ ios/iosUITests/iosUITests.swift
@@ -20,7 +20,7 @@ final class iosUITests: XCTestCase {
         XCTAssertTrue(checkButton.waitForExistence(timeout: 10), "The 'Check Status' button was not found")
         checkButton.tap()
 
-        let statusText = app.staticTexts["Status: ok\nVersion: 0.0.1-SNAPSHOT"]
+        let statusText = app.staticTexts["Status: ok\nVersion: 0.0.2-SNAPSHOT"]
         XCTAssertTrue(statusText.waitForExistence(timeout: 10), "Expected status text was not found")
     }
 }
