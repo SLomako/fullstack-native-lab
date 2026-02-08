1b10992b2ac2c9d623af7a5094bcb7d28a485e35
slomako <s.lomako@venga.com>
2026-02-08 21:35:11 +0300

    ios - ci3/ref61

--- ios/iosUITests/iosUITests.swift
+++ ios/iosUITests/iosUITests.swift
@@ -19,6 +19,6 @@ final class iosUITests: XCTestCase {
         XCTAssertTrue(checkButton.waitForExistence(timeout: 30))
         checkButton.tap()
 
-        XCTAssertTrue(app.staticTexts["Status: ok\nVersion: 0.0.1-SNAPSHOT"].waitForExistence(timeout: 30))
+        XCTAssertTrue(app.staticTexts["Status: ok\nVersion: 0.0.2-SNAPSHOT"].waitForExistence(timeout: 30))
     }
 }
