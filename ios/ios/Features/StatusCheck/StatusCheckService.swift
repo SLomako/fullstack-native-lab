930f4da7ca8d72e380d4065bbb63d07d8ccb47dd
Sergei Lomako <lomakosv@gmail.com>
2026-02-08 22:42:54 +0300

    ios - add run Ui and unit tests
    * ios - ci3/ref
    
    * ios - ci3/ref
    
    * ios - ci3/ref3
    
    * ios - ci3/ref4
    
    * ios - ci3/ref5
    
    * ios - ci3/ref6
    
    * ios - ci3/ref7
    
    * ios - ci3/ref8
    
    * ios - ci3/ref9
    
    * ios - ci3/ref10
    
    * ios - ci3/ref11
    
    * ios - ci3/ref12
    
    * fix
    
    * ios - ci3/ref13
    
    * ios - ci3/ref14
    
    * ios - ci3/ref15
    
    * ios - ci3/ref16
    
    * ios - ci3/ref17
    
    * ios - ci3/ref18
    
    * ios - ci3/ref19
    
    * ios - edit schema
    
    * ios - fix edit schema
    
    * ios - fix tests
    
    * ios - ci3/ref20
    
    * ios - ci3/ref21
    
    * ios - fix tests
    
    * ios - ci3/ref22
    
    * ios - ci3/ref23
    
    * ios - fix tests
    
    * ios - ci3/ref24
    
    * ios - ci3/ref25
    
    * ios - fix url
    
    * ios - fix tests
    
    * ios - fix tests
    
    * ios - ci3/ref26
    
    * ios - ci3/ref27
    
    * ios - ci3/ref28
    
    * ios - ci3/ref29
    
    * ios - ci3/ref30
    
    * ios - add info
    
    * ios - fix
    
    * ios - fix
    
    * ios - fix
    
    * ios - ci3/ref31
    
    * ios - ci3/ref32
    
    * ios - ci3/ref33
    
    * ios - ci3/ref34
    
    * ios - ci3/ref35
    
    * ios - ci3/ref36
    
    * ios - ci3/ref37
    
    * ios - ci3/ref38
    
    * ios - ci3/ref39
    
    * ios - ci3/ref40
    
    * ios - ci3/ref41
    
    * ios - ci3/ref42
    
    * ios - ci3/ref43
    
    * ios - ci3/ref44
    
    * ios - ci3/ref45
    
    * ios - ci3/ref46
    
    * ios - ci3/ref47
    
    * ios - ci3/ref48
    
    * ios - ci3/ref49
    
    * ios - ci3/ref50
    
    * ios - ci3/ref11111
    
    * ios - ci3/ref111111
    
    * ios - ci3/ref51
    
    * ios - ci3/fix1
    
    * ios - ci3/ref52
    
    * ios - ci3/ref53
    
    * ios - ci3/ref54
    
    * ios - ci3/ref55
    
    * ios - ci3/ref56
    
    * ios - ci3/ref57
    
    * ios - ci3/ref58
    
    * ios - ci3/ref59
    
    * ios - ci3/ref60
    
    * ios - ci3/ref61
    
    * ios - ci3/ref62
    
    * ios - ci3/ref63
    
    ---------
    
    Co-authored-by: slomako <s.lomako@venga.com>

--- ios/ios/Features/StatusCheck/StatusCheckService.swift
+++ ios/ios/Features/StatusCheck/StatusCheckService.swift
@@ -1,8 +1,18 @@
 import Foundation
 
 struct StatusCheckService {
+
+    private func backendHost() -> String {
+        let args = ProcessInfo.processInfo.arguments
+        if let i = args.firstIndex(of: "-backendHost"), i + 1 < args.count {
+            return args[i + 1]
+        }
+        return "127.0.0.1"
+    }
+
     func getStatus() async throws -> StatusCheckModel {
-        let url = URL(string: "http://localhost:8080/health")!
+        let host = backendHost()
+        let url = URL(string: "http://\(host):8080/health")!
         let (data, _) = try await URLSession.shared.data(from: url)
         return try StatusCheckParser.parse(data: data)
     }
